ff3b57bf2cbdbb901fa9ef55bf99f4d1433a50c1
Darshan-V <mr.darshanv@gmail.com>
2022-06-01 11:31:25 +0530

    if parser case 1

--- lisp.js
+++ lisp.js
@@ -1,6 +1,7 @@
+const readline = require('readline')
 let result
 const numberParser = input => (result = input.match(/^-?(0|([1-9][0-9]*))(\.[0-9]+)?([E][+-]?[0-9]+)?/i)) && [result[0] * 1, input.slice(result[0].length)]
-
+const spaceParser = input => input.replace(/^\s+/, '')
 const symbolParser = (input) => {
   result = input.match(/^(([a-zA-Z_]+)|(\+|-|>=|<=|>|<|=|\*|\\))/)
   if (!result) return null
@@ -21,82 +22,112 @@ const globalEnv = {
   sqrt: (input) => Math.sqrt(input)
 }
 
-const defineParser = input => {
-  if (!input.startsWith('define')) return null
-  input = input.slice(6).trim()
-  const result = symbolParser(input)
-  if (!result) return null
-  const identifyer = result[0]
-  const value = expressionParserEval(result[1])
-  if (!value) return null
-  input = value[1].trim()
-  if (input[0] !== ')') return null
-  globalEnv[identifyer] = value[0]
-  return [identifyer, value[1].slice(1).trim()]
-}
-
-const beginParser = (input, env) => {
-  let result
-  if (!input.startsWith('begin')) return null
-  input = input.slice(5)
-  while (input[0] !== ')') {
-    result = expressionParserEval(input, env)
-    input = result[1].trim()
-  }
-  return [result[0], input.slice(1)]
-}
-
 const ifParser = (input, env = globalEnv) => {
   if (!input.startsWith('if')) return null
   let result
-  input = input.slice(2).trim()
+  input = spaceParser(input.slice(2))
   result = sExpressionParser(input, env)
   if (!result) return null
   const condition = result[0]
-  input = result[1].trim()
+  input = spaceParser(result[1])
   if (condition) {
     result = expressionParserEval(input, env)
     if (!result) return null
     input = parseCode(result[1])[1]
     if (!input) return null
-    input = input.trim()
+    input = spaceParser(input)
     if (input[0] !== ')') return null
     return [result[0], input.slice(1)]
   }
   input = parseCode(result[1])[1]
   result = expressionParserEval(input, env)
   if (!result) return null
-  input = result[1].trim()
-  input.trim()
-  if (input !== ')') return null
+  input = spaceParser(result[1])
+  if (spaceParser(input) !== ')') return null
   return [result[0], input.slice(1)]
 }
 
-const globalEnvParser = (input, env = globalEnv) => {
-  const args = []
+const quoteParser = input => {
   let result
+  if (!input.startsWith('quote')) return null
+  input = input.slice(5)
+  input = spaceParser(input)
+  if (input[0] !== '(') {
+    result = ''
+    while (input[0] !== ')') {
+      result = result + input[0]
+      input = input.slice(1)
+    }
+    return [result, input.slice(1)]
+  }
+  result = parseCode(input)
+  if (!result) return null
+  input = spaceParser(result[1])
+  if (input[0] !== ')') return null
+  return [result[0], result[1].slice(1)]
+}
+
+const defineParser = input => {
+  if (!input.startsWith('define')) return null
+  input = input.slice(6)
+  input = spaceParser(input)
+  const result = symbolParser(input)
+  if (!result) return null
+  const identifier = result[0]
+  const value = expressionParserEval(result[1])
+  if (!value) return null
+  input = spaceParser(value[1])
+  if (input[0] !== ')') return null
+  globalEnv[identifier] = value[0]
+  return [identifier, spaceParser(value[1]).slice(1)]
+}
+
+const beginParser = (input, env) => {
+  if (!input.startsWith('begin')) return null
+  input = input.slice(5)
+  while (input[0] !== ')') {
+    result = expressionParserEval(input, env)
+    input = spaceParser(result[1])
+  }
+  return [result[0], input.slice(1)]
+}
+
+const globalEnvParser = (input, env) => {
+  const args = []
   const firstEmptyspaceIndex = input.indexOf(' ')
   const textChar = input.slice(0, firstEmptyspaceIndex)
   if (!globalEnv[textChar]) return null
   if (typeof globalEnv[textChar] === 'object') {
-    input = input.slice(firstEmptyspaceIndex).trim()
+    input = spaceParser(input.slice(firstEmptyspaceIndex))
     return result
   }
   const operation = textChar
-  input = input.slice(firstEmptyspaceIndex).trim()
+  input = spaceParser(input.slice(firstEmptyspaceIndex))
   // parse the arguments
   while (input[0] !== ')') {
     result = expressionParserEval(input, env)
     if (!result) return null
     args.push(result[0])
-    input = result[1].trim
+    input = spaceParser(result[1])
   }
   return [globalEnv[operation](args), input.slice(1)]
 }
 
+const sExpressionParser = (input, env = globalEnv) => {
+  let result
+  if (input[0] !== '(') return null
+  input = spaceParser(input.slice(1))
+  const parsers = [defineParser, beginParser, ifParser, quoteParser, globalEnvParser]
+  for (const parser of parsers) {
+    result = parser(input, env)
+    if (result) return result
+  }
+  return null
+}
+
 const parseCode = input => {
   let result
-  input = input.trim()
+  input = spaceParser(input)
   if ((result = numberParser(input))) return [result[0], result[1]]
   else if ((result = symbolParser(input))) return [result[0], result[1]]
   if (input[0] === '(') {
@@ -108,35 +139,39 @@ const parseCode = input => {
       if (input[0] === ')') count--
       if (count === 0) {
         result = result + ')'
-        input = input.slice(1).trim()
+        input = input.slice(1)
+        input = spaceParser(input)
         return [result, input]
       }
       result = result + input[0]
       input = input.slice(1)
     }
   }
-  return null
-}
-const sExpressionParser = (input, env) => {
-  let result
-  if (input[0] !== '(') return null
-  input = input.slice(1).trim()
-  const parsers = [defineParser, beginParser, ifParser, globalEnvParser]
-  for (const parser of parsers) {
-    result = parser(input, env)
-    if (result) return result
-  }
+
   return null
 }
 
-const expressionParserEval = (input, env) => {
-  input = input.trim()
+const expressionParserEval = (input, env = globalEnv) => {
+  input = spaceParser(input)
   if ((result = sExpressionParser(input, env))) return [result[0], result[1]]
   if ((result = numberParser(input))) return [result[0], result[1]]
   if ((result = symbolParser(input))) {
+    if (!env[result[0]] !== undefined) {
+      return [env[result[0]], result[1]]
+    }
     if (!env[result[0]]) return null
     return [env[result[0]], result[1]]
   }
   return null
 }
-console.log(expressionParserEval('(begin (define x 5) (* pi (* x x)))'))
+
+let rl = readline.createInterface(process.stdin, process.stdout)
+rl.setPrompt('lispi> ')
+rl.prompt()
+rl.on('line', function(line) {
+  if (line === 'quit') rl.close()
+  console.log(expressionParserEval(line))
+  rl.prompt()
+}).on('close', function() {
+  process.exit(0)
+})
