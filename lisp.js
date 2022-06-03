9401862dc6c4d2f1d918aa2bfda53b7da642d49c
Darshan-V <mr.darshanv@gmail.com>
2022-06-03 13:41:55 +0530

    frist method

--- lisp.js
+++ lisp.js
@@ -1,3 +1,4 @@
+const readline = require('readline')
 let result
 const numberParser = input => (result = input.match(/^-?(0|([1-9][0-9]*))(\.[0-9]+)?([E][+-]?[0-9]+)?/i)) && [result[0] * 1, input.slice(result[0].length)]
 const spaceParser = input => input.replace(/^\s+/, '')
@@ -26,31 +27,40 @@ const ifParser = (input, env = globalEnv) => {
   let result
   input = spaceParser(input.slice(2))
   result = sExpressionParser(input, env)
+
   if (!result) return null
   const condition = result[0]
   input = spaceParser(result[1])
+
   if (condition) {
     result = expressionParserEval(input, env)
+
     if (!result) return null
     input = parseCode(result[1])[1]
+
     if (!input) return null
     input = spaceParser(input)
+
     if (input[0] !== ')') return null
     return [result[0], input.slice(1)]
   }
   input = parseCode(result[1])[1]
   result = expressionParserEval(input, env)
+
   if (!result) return null
   input = spaceParser(result[1])
+
   if (spaceParser(input) !== ')') return null
   return [result[0], input.slice(1)]
 }
 
 const quoteParser = input => {
   let result
+
   if (!input.startsWith('quote')) return null
   input = input.slice(5)
   input = spaceParser(input)
+
   if (input[0] !== '(') {
     result = ''
     while (input[0] !== ')') {
@@ -60,23 +70,29 @@ const quoteParser = input => {
     return [result, input.slice(1)]
   }
   result = parseCode(input)
+
   if (!result) return null
   input = spaceParser(result[1])
+
   if (input[0] !== ')') return null
   return [result[0], result[1].slice(1)]
 }
 
 const lambdaParser = input => {
   let result
+
   if (!input.startsWith('lambda')) return null
   input = input.slice(6)
   input = spaceParser(input)
+
   if (input[0] !== '(') return null
   input = spaceParser(input.slice(1))
   const localEnv = {}
   const args = {}
+
   while (input[0] !== ')') {
     result = symbolParser(input)
+
     if (!result) return null
     args[result[0]] = null
     input = spaceParser(result[1])
@@ -98,11 +114,14 @@ const defineParser = input => {
   input = input.slice(6)
   input = spaceParser(input)
   const result = symbolParser(input)
+
   if (!result) return null
   const identifier = result[0]
   const value = expressionParserEval(result[1])
+
   if (!value) return null
   input = spaceParser(value[1])
+
   if (input[0] !== ')') return null
   globalEnv[identifier] = value[0]
   return [identifier, spaceParser(value[1]).slice(1)]
@@ -111,6 +130,7 @@ const defineParser = input => {
 const beginParser = (input, env) => {
   if (!input.startsWith('begin')) return null
   input = input.slice(5)
+
   while (input[0] !== ')') {
     result = expressionParserEval(input, env)
     input = spaceParser(result[1])
@@ -124,12 +144,12 @@ const updateLambdaArgs = (func, input, env) => {
 
   while (input[0] !== ')') {
     expressionResult = expressionParserEval(input, env)
+
     if (!expressionResult) return null
     globalEnv[func].args[funcKeys[index]] = expressionResult[0]
     index++
     input = spaceParser(expressionResult[1])
   }
-
   globalEnv[func].args.__func__ = globalEnv[func]
   globalEnv[func].argsCallStack.push(Object.assign({}, globalEnv[func].args))
   input = input.slice(1)
@@ -140,8 +160,8 @@ const updateLambdaArgs = (func, input, env) => {
 }
 
 const lambdaEval = (func) => {
-  const callStackLastItemIndex = globalEnv[func].argsCallStack.length - 1
-  const result = expressionParserEval(globalEnv[func].body, globalEnv[func].argsCallStack[callStackLastItemIndex])
+  const callStackLastIndex = globalEnv[func].argsCallStack.length - 1
+  const result = expressionParserEval(globalEnv[func].body, globalEnv[func].argsCallStack[callStackLastIndex])
   return result[0]
 }
 
@@ -149,6 +169,7 @@ const globalEnvParser = (input, env) => {
   const args = []
   const firstEmptyspaceIndex = input.indexOf(' ')
   const textChar = input.slice(0, firstEmptyspaceIndex)
+
   if (!globalEnv[textChar]) return null
   if (typeof globalEnv[textChar] === 'object') {
     input = spaceParser(input.slice(firstEmptyspaceIndex))
@@ -160,6 +181,7 @@ const globalEnvParser = (input, env) => {
   // parse the arguments
   while (input[0] !== ')') {
     result = expressionParserEval(input, env)
+
     if (!result) return null
     args.push(result[0])
     input = spaceParser(result[1])
@@ -169,9 +191,12 @@ const globalEnvParser = (input, env) => {
 
 const sExpressionParser = (input, env = globalEnv) => {
   let result
+
   if (input[0] !== '(') return null
-  input = spaceParser(input.slice(1))
+  input = input.slice(1)
+  // input = spaceParser(input.slice(1))
   const parsers = [defineParser, beginParser, ifParser, quoteParser, lambdaParser, globalEnvParser]
+
   for (const parser of parsers) {
     result = parser(input, env)
     if (result) return result
@@ -184,6 +209,7 @@ const parseCode = input => {
   input = spaceParser(input)
   if ((result = numberParser(input))) return [result[0], result[1]]
   else if ((result = symbolParser(input))) return [result[0], result[1]]
+
   if (input[0] === '(') {
     result = '('
     let count = 1
@@ -217,3 +243,15 @@ const expressionParserEval = (input, env = globalEnv) => {
   }
   return null
 }
+// console.log(expressionParserEval('(begin (define fun(lambda (x) (if (> x 10) 1 2)))(fun 9))'))
+// console.log(expressionParserEval('(define fib (lambda (n) (if (< n 2) 1 (+ (fib (- n 1)) (fib (- n 2)))))))'))
+const rl = readline.createInterface(process.stdin, process.stdout)
+rl.setPrompt('lispi> ')
+rl.prompt()
+rl.on('line', function (line) {
+  if (line === 'quit') rl.close()
+  console.log(expressionParserEval(line))
+  rl.prompt()
+}).on('close', function () {
+  process.exit(0)
+})
